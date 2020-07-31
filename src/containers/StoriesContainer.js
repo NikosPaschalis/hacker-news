959ec090eb4276ff91a6f7c226b2c3319bb8b5a0
Νικόλαος Πασχάλης <n.pashalis@DYPE.LOCAL>
2020-07-31 09:05:55 +0300

    test

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {getStoryIds} from '../services/hnApi';
 import { Story } from '../components/Story';
-
+import {GlobalStyle, StoriesContainerWrapper} from '../styles/StoriesContainerStyles';
 
 function StoriesContainer() {
   const [storyIds,setStoryIds] = useState([]);
@@ -10,7 +10,15 @@ function StoriesContainer() {
     getStoryIds().then(data => setStoryIds(data));
     //getStory(22283486).then(data => console.log(data));
   },[]);
-  return storyIds.map(storyId => <Story key={storyId}  storyId= {storyId} />);
+  return (
+    <>
+    <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News</h1>
+        {storyIds.map(storyId => <Story key={storyId}  storyId= {storyId} />)}
+      </StoriesContainerWrapper>
+    </>
+  );
 }
 
 export default StoriesContainer;
